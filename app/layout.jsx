d7e04dca46450f906e1bb6ba8d2ddc010b057961
rvilothra <rvdpmptsp@gmail.com>
2025-09-17 17:03:14 +0700

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,33 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Layanan Publik Kab Bogor",
+  description: "Sistem Layanan Publik Mobile Responsive",
+  icons: {
+    icon: "/kabbogor.ico",
+  },
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 5,
+  userScalable: true,
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/kabbogor.ico" />
+      </head>
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
